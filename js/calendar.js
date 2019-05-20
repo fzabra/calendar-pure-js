8e608c3e48d3a3e277bc1789d36aa892f9f6abf5
fabricio.sobral <fabricio.sobral@fireking.com>
2019-05-20 01:21:24 -0300

    before agenda

--- js/calendar.js
+++ js/calendar.js
@@ -1,71 +1,50 @@
 today = new Date();
-// currentDay = today.getDay();
-// console.log(currentDay);
+currentDay = today.getDate();
 currentMonth = today.getMonth();
 currentYear = today.getFullYear();
-selectYear = document.getElementById("year");
-selectMonth = document.getElementById("month");
 
-months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+monthsOfYear = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-monthAndYear = document.getElementById("monthAndYear");
-showCalendar(currentMonth, currentYear);
-
-
-function showCalendar(month, year) {
-
-    let firstDay = (new Date(year, month)).getDay();
-    console.log(firstDay);
-
-    tbl = document.getElementById("calendar-body"); // body of the calendar
-
-    // clearing all previous cells
-    tbl.innerHTML = "";
-
-    // filing data about month and in the page via DOM.
-    monthAndYear.innerHTML = months[month] + " " + year;
-    // selectYear.value = year;
-    // selectMonth.value = month;
-
-    // creating all cells
-    let date = 1;
+function buildCalendar(currentMonth, currentYear){
+    let firstDayOfMonth = (new Date(currentYear, currentMonth)).getDay();
+    console.log(firstDayOfMonth);
+    tbody = document.getElementById("calendar-tbody");
+    console.log(tbody);
+    tbody.innerHTML = "";
+    monthYear.innerHTML = monthsOfYear[currentMonth] + " " + currentYear;
+    let dateDay = 1;
     for (let i = 0; i < 6; i++) {
         // creates a table row
         let row = document.createElement("tr");
 
         //creating individual cells, filing them up with data.
         for (let j = 0; j < 7; j++) {
-            if (i === 0 && j < firstDay) {
+            if (i === 0 && j < firstDayOfMonth) {
                 cell = document.createElement("td");
                 cellText = document.createTextNode("");
                 cell.appendChild(cellText);
                 row.appendChild(cell);
+                cell.classList.add("bg-day-off");
             }
-            else if (date > daysInMonth(month, year)) {
+            else if (dateDay > daysInMonth(currentMonth, currentYear)) {
                 break;
             }
-
             else {
                 cell = document.createElement("td");
-                cellText = document.createTextNode(date);
-                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-                    cell.classList.add("bg-info");
-                } // color today's date
+                cellText = document.createTextNode(dateDay);
+                if (dateDay === today.getDate() && currentYear === today.getFullYear() && currentMonth === today.getMonth()) {
+                    cell.classList.add("bg-day");
+                }
                 cell.appendChild(cellText);
                 row.appendChild(cell);
-                date++;
-
-                console.log(cellText);
+                dateDay++;
             }
-
-
         }
-
-        tbl.appendChild(row); // appending each row into calendar body.
+        tbody.appendChild(row); 
     }
-
 }
 
+buildCalendar(currentMonth, currentYear);
 
 function daysInMonth(iMonth, iYear) {
     return 32 - new Date(iYear, iMonth, 32).getDate();
